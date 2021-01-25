d84ee6581e1f1feaa4ea102d49287ab826e84feb
Juan Sebastián Andrade <mr.jekyll31no@gmail.com>
2021-01-25 07:31:00 -0500

    Add files via upload
    Continued fixing aleatorization error.

--- Distances/Scatterplot-forScriptMinMesH.py
+++ Distances/Scatterplot-forScriptMinMesH.py
@@ -537,7 +537,7 @@ for term in MinMeSHtoProj.keys():
     SignPos=int((len(Randdiff)*0.01))
 #We compare the p-values of the first two tests (with bonferroni correction) and check the aleatorization. For the
 #aleatorization we check if the distances is smaller than the 1% percentile of the random median distance distribution.
-    if b<0.01/(2*len(kwordtoProj)) and d<0.01/(2*len(kwordtoProj)) and Randdiff[SignPos]>actual_diff:
+    if b<0.01/(2*len(kwordtoProj)) and d<0.01/(2*len(kwordtoProj)) and (Randdiff[SignPosLow]>actual_diff or Randdiff[SignPosHigh]<actual_diff):
 #We print the terms which pass the criteria with their abundance.
         if Randdiff[SignPosLow]>actual_diff:
             where="low"
